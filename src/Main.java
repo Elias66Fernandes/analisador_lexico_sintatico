1fa119ab6cd1e7f1c6762ff927e99befbb21b0bf
Elias66Fernandes <francisco.elias@discente.ufma.br>
2025-10-13 01:56:00 -0300

    Atualizando o Parser para suportar o tipo number

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 
 
 public class Main {
-        public static void main(String[] args) {
-            String input = "8+5-7+9";
-            Parser p = new Parser(input.getBytes());
-            p.parse();
-
-        }
+    public static void main(String[] args) {
+        String input = "89+508-7+99";
+        Parser p = new Parser (input.getBytes());
+        p.parse();
+    }
 }
