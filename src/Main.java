ca84fe064a96db96ee69d40cf97d103ede1c20fa
Elias66Fernandes <francisco.elias@discente.ufma.br>
2025-10-14 15:51:15 -0300

    Solução final do analizador lexico sintatico

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
-
-
 public class Main {
     public static void main(String[] args) {
-        String input = "45  + preco - 876";
-        Parser p = new Parser (input.getBytes());
+
+        String input = """
+            let a = 42 + 2;
+            let b = 15 + 3;
+            print a + b;
+        """;
+
+        Parser p = new Parser(input.getBytes());
         p.parse();
+
+        System.out.println("--- Código Gerado ---");
+        System.out.println(p.output());
+
+        System.out.println("--- Código Interpretado ---");
+        Interpretador i = new Interpretador(p.output());
+        i.run();
+
     }
 }
+
